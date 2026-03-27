b08e97ba622ff1d7e2a6ccc4145c326a929d8b20
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2026-03-27 13:48:09 +0200

    Update User.java

--- user-service/src/main/java/com/nexus/wallet/userservice/model/User.java
+++ user-service/src/main/java/com/nexus/wallet/userservice/model/User.java
@@ -1,10 +1,14 @@
 package com.nexus.wallet.userservice.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,7 +27,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @Email(message = "Invalid email address")
+    @NotBlank(message = "Email is required")
+    @Column(unique = true)
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
     private String password;
 }
